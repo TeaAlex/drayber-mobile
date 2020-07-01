36a899c345099219367120a6c77fd6edae51c748
Redha <--replace-al>
2020-07-01 20:22:40 +0200

    test user context

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useContext} from 'react'
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,11 +11,16 @@ import {getColor} from 'tailwind-rn';
 import WS from "./src/components/WS";
 import UserProvider from "./src/context/UserContext";
 import Register from './src/components/Register';
+import {UserContext} from "./src/context/UserContext";
 
 function HomeScreen({ navigation }) {
+
+  const {user} = useContext(UserContext);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Text>Bonjour { user.user.firstname}</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
