98201943de4b066b09c82a2ea59c9bb23224705e
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-07-01 16:23:12 +0200

    add react native credit card , nav to add card
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,61 +1,66 @@
-import React, {useState, useContext} from 'react'
-import {View, TextInput, Button, Text, TouchableOpacity} from "react-native";
-import {api} from "../utils/api";
+import React, {useState, useContext} from 'react';
+import {View, TextInput, Button, Text, TouchableOpacity} from 'react-native';
+import {api} from '../utils/api';
 import AsyncStorage from '@react-native-community/async-storage';
-import {UserContext} from "../context/UserContext";
-
+import {UserContext} from '../context/UserContext';
 
 const Login = ({navigation}) => {
 
-  const [email, setEmail] = useState('flex@flaex.fr');
-  const [password, setPassword] = useState('aaa');
-  const {setUser} = useContext(UserContext);
+    const [email, setEmail] = useState('flex@flaex.fr');
+    const [password, setPassword] = useState('aaa');
+    const {setUser} = useContext(UserContext);
 
-  const onPress = async () => {
-    const body = {
-      uid: email,
-      password
-    }
+    const onPress = async () => {
+        const body = {
+            uid: email,
+            password
+        }
 
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(!re.test(email) || email.length === 0){
-      return alert("Votre email est invalide");
-    }
-    if(password.length === 0){
+        if(!re.test(email) || email.length === 0){
+            return alert("Votre email est invalide");
+        }
+        if(password.length === 0){
 
-      return alert("Veuillez renseigner un mot de passe");
-    }
-    const {token} = await api('POST', '/login', body);
-    try {
-      await AsyncStorage.setItem('token', token);
-      const user = await api('GET', '/users/current-user');
-      setUser(user);
-      navigation.navigate('Home');
-    } catch (e) {
-      console.error(e);
-    }
-  }
+            return alert("Veuillez renseigner un mot de passe");
+        }
+        const {token} = await api('POST', '/login', body);
+        try {
+            await AsyncStorage.setItem('token', token);
+            const user = await api('GET', '/users/current-user');
+            setUser(user);
+            navigation.navigate('Home');
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
-  return (
-    <View>
-      <View>
-        <Text>Email</Text>
-        <TextInput value={email} onChangeText={(text) => setEmail(text)}/>
-      </View>
-      <View>
-        <Text>Mot de passe</Text>
-        <TextInput value={password} secureTextEntry={true} onChangeText={(text) => setPassword(text)}/>
-      </View>
-      <Button title="Se connecter" onPress={onPress}/>
-
-      <View >
-                <Text >Pas encore de compte? </Text>
-                <Button title="Inscrivez vous" onPress={() => navigation.navigate('Register')}/>
+    return (
+        <View>
+            <View>
+                <Text>Email</Text>
+                <TextInput value={email} onChangeText={text => setEmail(text)} />
             </View>
-    </View>
-  )
+            <View>
+                <Text>Mot de passe</Text>
+                <TextInput
+                    value={password}
+                    secureTextEntry={true}
+                    onChangeText={text => setPassword(text)}
+                />
+            </View>
+            <Button title="Se connecter" onPress={onPress} />
 
-}
+            <View>
+                <Text>Pas encore de compte? </Text>
+                <Button
+                    title="Inscrivez vous"
+                    onPress={() => navigation.navigate('Register')}
+                />
+            </View>
+        </View>
+    );
+};
 
-export default Login
+export default Login;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,15 @@
-import React from "react";
-import tailwind from "tailwind-rn";
-import {View} from "react-native";
-import MenuBackButton from "./MenuBackButton";
-import MenuItem from "./MenuItem";
-import PersonSvg from "../assets/icons/person.svg";
-import CreditCardSvg from "../assets/icons/credit-card-outline.svg";
-import CarSvg from "../assets/icons/automobile.svg";
-import CloseSvg from "../assets/icons/close-square-outline.svg";
+import React from 'react';
+import tailwind from 'tailwind-rn';
+import {TouchableHighlight, View} from 'react-native';
+import MenuBackButton from './MenuBackButton';
+import MenuItem from './MenuItem';
+import PersonSvg from '../assets/icons/person.svg';
+import CreditCardSvg from '../assets/icons/credit-card-outline.svg';
+import CarSvg from '../assets/icons/automobile.svg';
+import CloseSvg from '../assets/icons/close-square-outline.svg';
 
 const Menu = ({navigation}) => {
-
-  const color = '#586CD9'
+  const color = '#586CD9';
 
   return (
     <View style={tailwind('bg-gray-100 h-full items-center w-full')}>
@@ -19,20 +18,22 @@ const Menu = ({navigation}) => {
       {/*</View>*/}
       <View style={tailwind('bg-blue-500 w-full')}>
         <MenuItem text={'Mon profil'}>
-          <PersonSvg width={24} height={24} fill={color}/>
+          <PersonSvg width={24} height={24} fill={color} />
         </MenuItem>
-        <MenuItem text={'Moyen de paiements'}>
-          <CreditCardSvg width={24} height={24} fill={color}/>
-        </MenuItem>
+        <TouchableHighlight onPress={() => navigation.navigate('AddSubscriptionScreen')}>
+          <MenuItem text={'Moyen de paiements'}>
+            <CreditCardSvg width={24} height={24} fill={color} />
+          </MenuItem>
+        </TouchableHighlight>
         <MenuItem text={'Devenir chauffeur'}>
-          <CarSvg width={24} height={24} fill={color}/>
+          <CarSvg width={24} height={24} fill={color} />
         </MenuItem>
         <MenuItem text={'Deconnexion'}>
-          <CloseSvg width={24} height={24} fill={color}/>
+          <CloseSvg width={24} height={24} fill={color} />
         </MenuItem>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
