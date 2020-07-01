298e1e04eeef279d375f0d8d3630058fe46f2a65
Kaboudou <kingamet91@gmail.com>
2020-07-01 22:57:19 +0200

    Add Profile

--- src/components/Register.js
+++ src/components/Register.js
@@ -35,6 +35,7 @@ const Register = ({navigation}) => {
       "email": email,
       "password": password,
       "password_confirmation": password_confirmation,
+      
       "firstname": firstname,
       "lastname": lastname,
       "phone_number":phone_number,
