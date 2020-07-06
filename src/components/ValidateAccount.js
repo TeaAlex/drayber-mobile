a04cba3d4b479f4d04b71648e28f5039c4c5dace
Redha <--replace-al>
2020-07-06 23:29:46 +0200

    update admin works

--- src/components/ValidateAccount.js
+++ src/components/ValidateAccount.js
@@ -26,6 +26,20 @@ const ValidateAccount = ({route,navigation}) => {
         getDriverData();
   }, []);
 
+  const validateAccount = async () => {
+
+    const body = {
+        active_driver:true
+    }
+
+      try {
+        await api('PUT', '/users/update/'+driver.user.id, body);
+        alert("Le compte "+driver.user.id+" a bien été validé");
+        navigation.navigate('Admin');
+      } catch (e) {
+          console.error(e);
+      }
+    }
 
   return (
      
@@ -53,7 +67,7 @@ const ValidateAccount = ({route,navigation}) => {
                 
                 
               <View style={tailwind('flex flex-row justify-center items-center')}>
-            <TouchableHighlight style={{...tailwind('bg-indigo-800 p-4 rounded mt-5')}} >
+            <TouchableHighlight style={{...tailwind('bg-indigo-800 p-4 rounded mt-5')}}  onPress={() => validateAccount()} >
               <Text style={tailwind('text-white font-bold text-center text-lg')}> Valider le compte </Text>
             </TouchableHighlight>
           </View>
