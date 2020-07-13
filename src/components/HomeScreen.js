feb4f9fa9a5a42ccab4e6c77074efa9ae0127724
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-07-13 18:57:47 +0200

    driver mode homescreen
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -5,10 +5,16 @@ import {Button, Text, View, TouchableOpacity, TextInput, TouchableHighlight} fro
 import FirebaseNotifHandler from './FirebaseNotifHandler';
 import tailwind from 'tailwind-rn';
 import MenuToggle from '../assets/icons/menu-toggle.svg';
+import MenuItem from './MenuItem';
 import MapView, {Polyline, PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import InputAddress from './InputAddress';
+import Swap from '../assets/icons/swap-outline.svg';
+import {showMessage, hideMessage} from 'react-native-flash-message';
+
 
 function HomeScreen({navigation}) {
+  const color = '#586CD9';
+
   const {user} = useContext(UserContext);
   const [mode, setMode] = useState('');
   const [currentPosition, setCurrentPosition] = useState(null);
@@ -32,9 +38,42 @@ function HomeScreen({navigation}) {
     getMode();
   }, []);
 
+
+  const switchMode = async () => {
+    const mode = await AsyncStorage.getItem('changeMode');
+    if(mode === "Client"){
+      if(user.driver){
+        console.log(user.driver.active_driver)
+        if(user.driver.active_driver === true){
+          await AsyncStorage.setItem('changeMode', "Driver");
+          navigation.navigate('Home')
+        }else {
+          showMessage({
+            message: 'Erreur',
+            description: "Votre compte Driver n'est pas encore activé.",
+            type: 'danger',
+            icon: 'danger',
+          });
+          // alert("Votre compte Driver n'est pas encore activé")
+        }
+      } else {
+        showMessage({
+          message: 'Erreur',
+          description: "Vous n'êtes pas chauffeur :( ",
+          type: 'danger',
+          icon: 'danger',
+        });
+        // alert("Vous n'êtes pas chauffeur :(")
+      }
+    } else {
+      await AsyncStorage.setItem('changeMode', "Client");
+    }
+
+  }
+
   return (
     <View style={tailwind('h-full w-full')}>
-          {user.user.role_id === 3 &&
+          {user.driver &&
           <TouchableOpacity
           style={{position: 'absolute', top: 40, right: 20, zIndex: 100}}
           onPress={() => navigation.navigate('Menu')}>
@@ -59,8 +98,8 @@ function HomeScreen({navigation}) {
 
       <View style={tailwind('w-full items-center mt-4 p-2')}>
 
-      {user.user.role_id === 3 &&
-            <TouchableHighlight style={tailwind('items-center p-3 w-2/3 bg-indigo-800 p-4 rounded')}>
+      {user.driver  &&
+            <TouchableHighlight style={tailwind('items-center p-3 w-2/3 bg-indigo-800 p-4 mb-10 rounded')}>
            <Text style={tailwind('text-white font-bold text-center text-lg')}>
             {' '} 
             En attente d'un client ... ⏳{' '}
@@ -68,7 +107,8 @@ function HomeScreen({navigation}) {
         </TouchableHighlight>
         }
 
-        {user.user.role_id === 2 &&
+        {/* {user.driver.active_driver === false && */}
+
         <TouchableHighlight
             style={tailwind('items-center p-3 w-2/3 bg-indigo-800 p-4 rounded')}
             onPress={() => navigation.navigate('Search')}>
@@ -77,7 +117,11 @@ function HomeScreen({navigation}) {
             Où allez vous ? {' '}
           </Text>
         </TouchableHighlight>
-        }
+        
+        {/* } */}
+
+        
+
       </View>
 
       {/* <Text>Home Screen</Text>
