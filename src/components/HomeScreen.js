a901bf7963a4da582e7c84da5a6e9357de49bc83
OmarAbdallaa <omar.abdallaa@gmail.com>
2020-07-13 17:32:31 +0200

    homescreen
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,14 +1,28 @@
 import React, {useContext, useState, useEffect} from 'react';
-import { UserContext } from "../context/UserContext";
-import AsyncStorage from "@react-native-community/async-storage";
-import { Button, Text, View } from "react-native";
-import FirebaseNotifHandler from "./FirebaseNotifHandler";
+import {UserContext} from '../context/UserContext';
+import AsyncStorage from '@react-native-community/async-storage';
+import {Button, Text, View, TouchableOpacity, TextInput, TouchableHighlight} from 'react-native';
+import FirebaseNotifHandler from './FirebaseNotifHandler';
+import tailwind from 'tailwind-rn';
+import MenuToggle from '../assets/icons/menu-toggle.svg';
+import MapView, {Polyline, PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import InputAddress from './InputAddress';
 
-function HomeScreen({ navigation }) {
-
+function HomeScreen({navigation}) {
   const {user} = useContext(UserContext);
-
   const [mode, setMode] = useState('');
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 48.8587741,
+    longitude: 2.2069771,
+    latitudeDelta: 0.04,
+    longitudeDelta: 0.04,
+  });
+
+  const [height, setHeight] = useState({
+    map: '85%',
+    info: '52%',
+  });
 
   useEffect(() => {
     async function getMode() {
@@ -19,8 +33,54 @@ function HomeScreen({ navigation }) {
   }, []);
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={tailwind('h-full w-full')}>
+          {user.user.role_id === 3 &&
+          <TouchableOpacity
+          style={{position: 'absolute', top: 40, right: 20, zIndex: 100}}
+          onPress={() => navigation.navigate('Menu')}>
+          <View style={tailwind('items-center p-3  bg-indigo-800 p-4 rounded')}> 
+          <Text style={tailwind('text-white font-bold text-center text-lg')}>Solde :</Text>
+          </View>
+        </TouchableOpacity>
+}
+
+      <TouchableOpacity
+        style={{position: 'absolute', top: 40, left: 20, zIndex: 100}}
+        onPress={() => navigation.navigate('Menu')}>
+        <MenuToggle />
+      </TouchableOpacity>
+      <MapView
+        style={{...tailwind('w-full'), height: height.map}}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        loadingEnabled={true}
+        region={region}
+      />
+
+      <View style={tailwind('w-full items-center mt-4 p-2')}>
+
+      {user.user.role_id === 3 &&
+            <TouchableHighlight style={tailwind('items-center p-3 w-2/3 bg-indigo-800 p-4 rounded')}>
+           <Text style={tailwind('text-white font-bold text-center text-lg')}>
+            {' '} 
+            En attente d'un client ... ⏳{' '}
+          </Text>
+        </TouchableHighlight>
+        }
+
+        {user.user.role_id === 2 &&
+        <TouchableHighlight
+            style={tailwind('items-center p-3 w-2/3 bg-indigo-800 p-4 rounded')}
+            onPress={() => navigation.navigate('Search')}>
+          <Text style={tailwind('text-white font-bold text-center text-lg')}>
+            {' '}
+            Où allez vous ? {' '}
+          </Text>
+        </TouchableHighlight>
+        }
+      </View>
+
+      {/* <Text>Home Screen</Text>
       <Text> Mode {mode}</Text>
       <Text>Bonjour {user.user.firstname}</Text>
       <Button
@@ -38,10 +98,10 @@ function HomeScreen({ navigation }) {
       <Button
         title="Go to Menu"
         onPress={() => navigation.navigate('Menu')}
-      />
-      <FirebaseNotifHandler/>
+      /> */}
+      <FirebaseNotifHandler />
     </View>
   );
 }
 
-export default HomeScreen
+export default HomeScreen;
