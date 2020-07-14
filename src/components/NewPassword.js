c033591b06334b9a0b5a01a89ad0c4f25a9c2597
Redha <--replace-al>
2020-07-14 14:42:44 +0200

    fix reset password

--- src/components/NewPassword.js
+++ src/components/NewPassword.js
@@ -1,23 +1,18 @@
-import React, {useContext, useState} from 'react';
+import React, { useState} from 'react';
 import tailwind from 'tailwind-rn';
 import {
   Text,
   StyleSheet,
-  TouchableOpacity,
   View,
   TextInput,
   TouchableHighlight,
-  Button,
   ScrollView,
 } from 'react-native';
-import {UserContext} from '../context/UserContext';
 import {api} from '../utils/api';
-import {showMessage, hideMessage} from 'react-native-flash-message';
-
+import {showMessage} from 'react-native-flash-message';
+import Input from "./Input";
 
 const NewPassword = ({navigation}) => {
-  const {user} = useContext(UserContext);
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [token, setToken] = useState('');
   const [password_confirmation, setPasswordConfirm] = useState('');
@@ -25,6 +20,7 @@ const NewPassword = ({navigation}) => {
   const onPress = async () => {
     const body = {
       password: password,
+      password_confirmation: password_confirmation,
       token: token,
     };
     console.log(body);
@@ -56,8 +52,14 @@ const NewPassword = ({navigation}) => {
 
     }
     try {
-      await api('PUT', '/update-password', body);
+      await api('POST', '/update-password-by-token', body);
       navigation.navigate('Login');
+      return showMessage({
+        message: 'Succès',
+        description: "Votre mot de passe à été réinitialisé avec succès.",
+        type: 'success',
+        icon: 'success',
+      });
     } catch (e) {
       console.error(e);
     }
@@ -65,42 +67,14 @@ const NewPassword = ({navigation}) => {
 
   return (
     <ScrollView>
-      <View style={styles.backgroundContainer}>
+      <View  style={tailwind('w-full flex items-center mt-12')}>
         <Text>Réinitialiser votre mot de passe</Text>
-        <View style={styles.container}>
-          <Text>Token</Text>
-          <TextInput
-            style={tailwind(
-              'w-full bg-white border-2 border-gray-400 p-3 rounded text-gray-700 font-bold',
-            )}
-            value={token}
-            secureTextEntry={true}
-            onChangeText={text => setToken(text)}
-          />
-        </View>
-        <View style={styles.container}>
-          <Text>Nouveau mot de passe</Text>
-          <TextInput
-            style={tailwind(
-              'w-full bg-white border-2 border-gray-400 p-3 rounded text-gray-700 font-bold',
-            )}
-            value={password}
-            secureTextEntry={true}
-            onChangeText={text => setPassword(text)}
-          />
-        </View>
-        <View style={styles.container}>
-          <Text>Confirmer mot de passe</Text>
-          <TextInput
-            style={tailwind(
-              'w-full bg-white border-2 border-gray-400 p-3 rounded text-gray-700 font-bold',
-            )}
-            value={password_confirmation}
-            secureTextEntry={true}
-            onChangeText={text => setPasswordConfirm(text)}
-          />
-        </View>
-        <TouchableHighlight
+        <Input label={"Token"} value={token} onChange={text => setToken(text)} />
+        <Input label={"Nouveau mot de passe"} value={password} onChange={text => setPassword(text)} placeholder={"*****"} secureTextEntry={true} />
+        <Input label={"Confirmer mot de passe"} value={password_confirmation} onChange={text => setPasswordConfirm(text)} placeholder={"*****"} secureTextEntry={true} />
+
+      <View  style={tailwind('mt-12')}>
+      <TouchableHighlight
           style={tailwind('bg-indigo-800 p-4 rounded')}
           onPress={onPress}>
           <Text style={tailwind('text-white font-bold text-center text-lg')}>
@@ -109,24 +83,9 @@ const NewPassword = ({navigation}) => {
           </Text>
         </TouchableHighlight>
       </View>
+      </View>
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    marginVertical: 12,
-    paddingTop: 10,
-  },
-  backgroundContainer: {
-    padding: 20,
-    width: '100%',
-    maxWidth: 340,
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default NewPassword;
