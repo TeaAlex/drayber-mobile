3529c9d57e449506383db93dcbfc0645e0a43bd2
Alex Tea <alextea2@gmail.com>
2020-07-14 13:37:11 +0200

    redesign driver Homescreen + add conditions

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,12 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 import AsyncStorage from '@react-native-community/async-storage';
-import { Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { Text, View, TouchableOpacity, TouchableHighlight, ActivityIndicator } from 'react-native';
 import FirebaseNotifHandler from './FirebaseNotifHandler';
 import tailwind, { getColor } from 'tailwind-rn';
 import MenuToggle from '../assets/icons/menu-toggle.svg';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import { showMessage } from 'react-native-flash-message';
 import SearchIcon from '../assets/icons/search-outline.svg'
 import HomeIcon from '../assets/icons/home-outline.svg'
 import { getCurrentPosition, requestLocationPermission, search } from "../utils/geolocation";
@@ -31,6 +30,7 @@ function HomeScreen ({navigation}) {
 
 
   useEffect(() => {
+    console.log(user);
     async function getMode () {
       setMode(await AsyncStorage.getItem('changeMode'));
     }
@@ -76,8 +76,10 @@ function HomeScreen ({navigation}) {
         <TouchableOpacity
           style={{position: 'absolute', top: 40, right: 20, zIndex: 100}}
           onPress={() => navigation.navigate('Menu')}>
-          <View style={tailwind('items-center p-3  bg-indigo-800 p-4 rounded')}>
-            <Text style={tailwind('text-white font-bold text-center text-lg')}>Solde :</Text>
+          <View style={tailwind('items-center p-3  bg-indigo-800 p-4 rounded-full')}>
+            <Text style={tailwind('text-indigo-100 font-bold text-center text-base')}>
+              {user.driver.credit} €
+            </Text>
           </View>
         </TouchableOpacity>
       }
@@ -102,56 +104,60 @@ function HomeScreen ({navigation}) {
 
       <View style={{...tailwind('w-full items-center relative bg-gray-900 rounded-t-lg'), 'height': '20%'}}>
 
-        {user.driver &&
-        <TouchableHighlight style={tailwind('items-center p-3 w-2/3 bg-indigo-800 p-4 mb-10')}>
-          <Text style={tailwind('text-white font-bold text-center text-lg')}>
-            {' '}
-            En attente d'un client ... ⏳{' '}
-          </Text>
-        </TouchableHighlight>
+        { user.driver && user.driver.is_searching &&
+          <TouchableHighlight style={tailwind('items-center p-3 w-2/3 bg-white rounded-lg p-4 mt-10')}>
+            <View>
+              <Text style={tailwind('text-gray-700 font-bold text-center text-lg')}>
+                En attente d'un client ...
+              </Text>
+              <ActivityIndicator/>
+            </View>
+          </TouchableHighlight>
         }
 
-        <TouchableHighlight
-          style={{
-            ...tailwind('items-center p-3 w-10/12 bg-white p-6 rounded-lg absolute'),
-            top: '-20%',
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 5,
-            },
-            shadowOpacity: 0.34,
-            shadowRadius: 6.27,
-            elevation: 10,
-          }}
-          onPress={() => navigation.navigate('Search')}>
-          <View style={tailwind('flex flex-row justify-center w-full relative')}>
-            <View style={{...tailwind('absolute w-full'), 'left': '2%'}}>
-              <SearchIcon width={24} height={24} fill={getColor('gray-700')}/>
-            </View>
-            <Text style={tailwind('text-gray-800 font-bold text-center text-lg')}>
-              Où allez vous ?
-            </Text>
-          </View>
-        </TouchableHighlight>
+        {
+          user.driver === null || user.driver.is_searching === false &&
+            <TouchableHighlight
+              style={{
+                ...tailwind('items-center p-3 w-10/12 bg-white p-6 rounded-lg absolute'),
+                top: '-20%',
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 5,
+                },
+                shadowOpacity: 0.34,
+                shadowRadius: 6.27,
+                elevation: 10,
+              }}
+              onPress={() => navigation.navigate('Search')}>
+              <View style={tailwind('flex flex-row justify-center w-full relative')}>
+                <View style={{...tailwind('absolute w-full'), 'left': '2%'}}>
+                  <SearchIcon width={24} height={24} fill={getColor('gray-700')}/>
+                </View>
+                <Text style={tailwind('text-gray-800 font-bold text-center text-lg')}>
+                  Où allez vous ?
+                </Text>
+              </View>
+            </TouchableHighlight>
+        }
 
-        <TouchableHighlight
-          style={{...tailwind('bg-white rounded-lg p-4 absolute'), top: '45%'}}
-          onPress={goHome}
-        >
-          <View style={tailwind('flex flex-row justify-center')}>
-            <View style={tailwind('mr-2')}>
-              <HomeIcon width={24} height={24} fill={getColor('gray-700')}/>
-            </View>
-            {
-              user && user.user && user.user.address &&
-              <Text style={tailwind('text-gray-800 font-bold text-center text-lg')}>
-                Domicile
-              </Text>
-            }
-          </View>
-        </TouchableHighlight>
-
+        {
+          (user.driver === null || user.driver.is_searching === false) && (user.user.address) &&
+            <TouchableHighlight
+              style={{...tailwind('bg-white rounded-lg p-4 absolute'), top: '45%'}}
+              onPress={goHome}
+            >
+              <View style={tailwind('flex flex-row justify-center')}>
+                <View style={tailwind('mr-2')}>
+                  <HomeIcon width={24} height={24} fill={getColor('gray-700')}/>
+                </View>
+                <Text style={tailwind('text-gray-800 font-bold text-center text-lg')}>
+                  Domicile
+                </Text>
+              </View>
+            </TouchableHighlight>
+        }
       </View>
       <FirebaseNotifHandler/>
     </View>
