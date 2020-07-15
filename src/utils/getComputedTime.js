d0df50fe94d4f67f7cbf1b0820d72cc2dc4aeb5d
Alex Tea <alextea2@gmail.com>
2020-07-15 10:45:46 +0200

    fix getComputedTime format

--- src/utils/getComputedTime.js
+++ src/utils/getComputedTime.js
@@ -1,5 +1,7 @@
 export const getComputedTime = (seconds) => {
   let date = new Date();
   date.setSeconds(date.getSeconds() + seconds);
-  return `${date.getHours()}h${date.getMinutes()}`;
+  const hours = date.getHours().toString().length === 1 ? `0${date.getHours().toString()}` : date.getHours();
+  const minutes = date.getMinutes().toString().length === 1 ? `0${date.getMinutes().toString()}` : date.getMinutes();
+  return `${hours}h${minutes}`;
 }
